1ceb4ded55e81a3caf040e10702cad96906161c9
ReemyAhmad <remyahmad222@gmail.com>
2023-12-29 13:45:31 +0300

    relations and fillable and casts

--- newproject/app/Models/Company.php
+++ newproject/app/Models/Company.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Ticket;
 
 class Company extends Model
 {
     use HasFactory;
+    // name
+    // phone
+    protected $table = 'companies';
+    
+    protected $fillable =[
+        'name',
+        'phone',
+    ];
+    
+    protected $casts = [
+        'name'=>'string',
+        'phone'=>'string',
+    ];
+
+    public function tickets() {
+        return $this->hasMany(Ticket::class);
+    }
+
 }
